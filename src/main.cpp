be1a5ac878b7d5073907ab4c810c8573c2d61e60
Svetlozar Georgiev <s.georgiev255@gmail.com>
2020-05-23 23:33:07 +0100

    Add vkInstance

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <GLFW/glfw3.h>
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+
+#define SHOW_SUPPORTED_EXTENSIONS 0
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
+    GLFWwindow* window_;
+    VkInstance instance_;
+
 public:
     void run() {
         initWindow();
@@ -28,7 +35,48 @@ private:
         window_ = glfwCreateWindow(WIDTH, HEIGHT, "Hello, Vulkan!", nullptr, nullptr);
     }
 
-    void initVulkan() { }
+    void createInstance() {
+        VkApplicationInfo appInfo {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello, Vulkan!";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // Get supported extensions
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+#if SHOW_SUPPORTED_EXTENSIONS
+        std::cout << "Available extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+#endif
+
+        // Get extensions needed by GLFW
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        // Setup instance
+        VkInstanceCreateInfo createInfo {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        // Pass the extensions required by GLFW
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        // Global validation layers to enable
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance_) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create Vulkan instance!");
+        }
+    }
+
+    void initVulkan() { createInstance(); }
 
     void mainLoop() {
         while (!glfwWindowShouldClose(window_)) {
@@ -37,11 +85,11 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance_, nullptr);
+
         glfwDestroyWindow(window_);
         glfwTerminate();
     }
-
-    GLFWwindow* window_;
 };
 
 int main() {

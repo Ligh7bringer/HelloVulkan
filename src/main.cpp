888ee83b0f689ddc083435a1170ecd3cc4673435
Svetlozar Georgiev <s.georgiev255@gmail.com>
2020-05-26 00:02:12 +0100

    Setup pipeline parameters

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@ class HelloTriangleApplication {
     VkFormat                 swapChainImageFormat_;
     VkExtent2D               swapChainExtent_;
     std::vector<VkImageView> swapChainImageViews_;
+    VkPipelineLayout         pipelineLayout_;
 
 public:
     void run() {
@@ -85,6 +86,7 @@ private:
             vkDestroyImageView(device_, imageView, nullptr);
         }
 
+        vkDestroyPipelineLayout(device_, pipelineLayout_, nullptr);
         vkDestroySwapchainKHR(device_, swapChain_, nullptr);
         vkDestroyDevice(device_, nullptr);
         vkDestroySurfaceKHR(instance_, surface_, nullptr);
@@ -324,6 +326,100 @@ private:
 
         VkPipelineShaderStageCreateInfo shaderStages[]
             = { vertShaderStageInfo, fragShaderStageInfo };
+
+        VkPipelineVertexInputStateCreateInfo vertexInputInfo {};
+        vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertexInputInfo.vertexBindingDescriptionCount   = 0;
+        vertexInputInfo.pVertexBindingDescriptions      = nullptr; // Optional
+        vertexInputInfo.vertexAttributeDescriptionCount = 0;
+        vertexInputInfo.pVertexAttributeDescriptions    = nullptr; // Optional
+
+        // Setup geometry kind & and primitive restart
+        VkPipelineInputAssemblyStateCreateInfo inputAssembly {};
+        inputAssembly.sType    = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+        VkViewport viewport {};
+        viewport.x        = 0.f;
+        viewport.y        = 0.f;
+        viewport.width    = (float)swapChainExtent_.width;
+        viewport.height   = (float)swapChainExtent_.height;
+        viewport.minDepth = 0.f;
+        viewport.maxDepth = 1.f;
+
+        // We want to draw to the entire framebuffer - use a scissor rectangle with the same size
+        VkRect2D scissor {};
+        scissor.offset = { 0, 0 };
+        scissor.extent = swapChainExtent_;
+
+        VkPipelineViewportStateCreateInfo viewportState {};
+        viewportState.sType         = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        viewportState.viewportCount = 1;
+        viewportState.pViewports    = &viewport;
+        viewportState.scissorCount  = 1;
+        viewportState.pScissors     = &scissor;
+
+        VkPipelineRasterizationStateCreateInfo rasterizer {};
+        rasterizer.sType            = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        rasterizer.depthClampEnable = VK_FALSE;
+        rasterizer.rasterizerDiscardEnable = VK_FALSE;
+        rasterizer.polygonMode             = VK_POLYGON_MODE_FILL;
+        rasterizer.lineWidth               = 1.f;
+        rasterizer.cullMode                = VK_CULL_MODE_BACK_BIT;
+        rasterizer.frontFace               = VK_FRONT_FACE_CLOCKWISE;
+        rasterizer.depthBiasEnable         = VK_FALSE;
+        rasterizer.depthBiasConstantFactor = 0.f; // Optional (used if above is enabled)
+        rasterizer.depthBiasClamp          = 0.f; // As above
+        rasterizer.depthBiasSlopeFactor    = 0.f; // As above
+
+        // Multisampling can be used for anitalising
+        // Note: enabling it requires a GPU feature
+        VkPipelineMultisampleStateCreateInfo multisampling {};
+        multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisampling.sampleShadingEnable   = VK_FALSE;
+        multisampling.rasterizationSamples  = VK_SAMPLE_COUNT_1_BIT;
+        multisampling.minSampleShading      = 1.f; // Optional
+        multisampling.pSampleMask           = nullptr; // Optional
+        multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
+        multisampling.alphaToOneEnable      = VK_FALSE; // Optional
+
+        // VkPipelineColorBlendAttachmentState -> per attached framebuffer
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT
+            | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+        colorBlendAttachment.blendEnable         = VK_FALSE;
+        colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+        colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+        colorBlendAttachment.colorBlendOp        = VK_BLEND_OP_ADD; // Optional
+        colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+        colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+        colorBlendAttachment.alphaBlendOp        = VK_BLEND_OP_ADD; // Optional
+
+        // VkPipelineColorBlendStateCreateInfo -> global colour blending settings
+        VkPipelineColorBlendStateCreateInfo colorBlending {};
+        colorBlending.sType             = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        colorBlending.logicOpEnable     = VK_FALSE;
+        colorBlending.logicOp           = VK_LOGIC_OP_COPY; // Optional
+        colorBlending.attachmentCount   = 1;
+        colorBlending.pAttachments      = &colorBlendAttachment;
+        colorBlending.blendConstants[0] = 0.0f; // Optional
+        colorBlending.blendConstants[1] = 0.0f; // Optional
+        colorBlending.blendConstants[2] = 0.0f; // Optional
+        colorBlending.blendConstants[3] = 0.0f; // Optional
+
+        // Note: Dynamic state can be specified here so that pipeline re-creation is not necessary
+        // to change those values
+
+        // Setup shader uniforms - currently none used
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo {};
+        pipelineLayoutInfo.sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount         = 0;
+        pipelineLayoutInfo.pSetLayouts            = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 0;
+        pipelineLayoutInfo.pPushConstantRanges    = nullptr;
+
+        VK_SAFE(vkCreatePipelineLayout(device_, &pipelineLayoutInfo, nullptr, &pipelineLayout_));
 
         // Create pipeline...
 
